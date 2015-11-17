8d7fbbfae9690a66a8fcde1652cf85ae3feb03b7
Xearox <support@xearox.de>
2015-11-17 07:21:25 +0100

    UtilClass modified
    Added some methods to the class
    
    +listHome
    +createFile with String or File
    +fileExist with String or File

--- xHome/src/main/java/de/xearox/xplugin/UtilClass.java
+++ xHome/src/main/java/de/xearox/xplugin/UtilClass.java
@@ -5,6 +5,8 @@ package de.xearox.xplugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -18,7 +20,7 @@ public class UtilClass {
 	
 	/**
 	 * 
-	 * @param fileName
+	 * @param fileName String
 	 * @return if the File exist or not
 	 */
 	public boolean fileExist(String fileName){
@@ -28,6 +30,19 @@ public class UtilClass {
 		} else {
 			return false;
 		}
+	}
+	/**
+	 * 
+	 * @param filename File
+	 * @return if the File exist or not
+	 */
+	public boolean fileExist(File filename){
+		if(filename.exists()){
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 	/**
 	 * 
@@ -47,19 +62,30 @@ public class UtilClass {
 		return player.getUniqueId().toString();
 	}
 	
-	public void listHome(Player player){
+	public void listHome(Plugin plugin, Player player){
 	
-		fileExist("");
+		String filePath = "/data/";
+		String fileName = "homelist";
+		String fileType = ".yml";
+		File homefile = new File(plugin.getDataFolder()+File.separator+filePath+fileName+fileType);
+		if(!fileExist(homefile)){
+			createFile(homefile);
+		}
+		YamlConfiguration yamlFile = YamlConfiguration.loadConfiguration(homefile);
+		player.sendMessage(yamlFile.getConfigurationSection("Player."+ player.getUniqueId().toString()).getKeys(false).toString());
+		
 	}
 	/**
 	 *  
 	 * @param plugin
+	 * @param filePath The path of the File. It can be empty
 	 * @param fileName The name of the File
+	 * @param fileType The type of the File. e.g. .yml
 	 */
-	public void createFile(Plugin plugin, String fileName){
-		File newFile = new File(plugin.getDataFolder()+File.separator+fileName+".yml");
+	public void createFile(Plugin plugin,String filePath, String fileName, String fileType){
+		File newFile = new File(plugin.getDataFolder()+File.separator+fileName+fileType);
 		YamlConfiguration yamlFile = YamlConfiguration.loadConfiguration(newFile);
-		if(!fileExist(plugin.getDataFolder()+File.separator+fileName+".yml")){
+		if(!fileExist(plugin.getDataFolder()+File.separator+filePath+fileName+fileType)){
 			try {
 				yamlFile.save(newFile);
 			} catch (IOException e) {
@@ -67,12 +93,55 @@ public class UtilClass {
 				e.printStackTrace();
 			}
 			
-		}else {
-			System.out.println("The file "+fileName+" does already exist. No operation needed");
+		}else{
+			System.out.println("The file "+fileName+" does already exist. No operation needed!");
 		}
 		
 	}
 	
+	public void createFile(File file){
+		YamlConfiguration yamlFile = YamlConfiguration.loadConfiguration(file);
+		if(!fileExist(file)){
+			try {
+				yamlFile.save(file);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else{
+			System.out.println("The file "+file.getName().toString()+" doeas already exist. No operation needed!");
+		}
+	}
+	
+	/**
+	 * 
+	 * @param p Player
+	 * @return the Language of the players client
+	 */
+	private String getPlayerLanguage(Player p){		
+		try {
+			Object obj = getMethod("getHandle", p.getClass()).invoke(p, (Object[]) null);
+			Field f = obj.getClass().getDeclaredField("locale");
+			f.setAccessible(true);
+			String language = (String) f.get(obj);
+			return language;
+		} catch (Exception e) {
+			return "en";
+		}
+	}
+	/**
+	 * 
+	 * @param n
+	 * @param c
+	 * @return
+	 */
+	private Method getMethod(String n, Class<?> c) {
+		for (Method m : c.getDeclaredMethods()) {
+			if (m.getName().equals(n))
+				return m;
+		}
+		return null;
+	}
 	
 	
 	
