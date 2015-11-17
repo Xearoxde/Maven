f075a0cb39a5f67c2efa4924c1ceaaf4f9426d59
Xearox <support@xearox.de>
2015-11-17 10:30:31 +0100

    Added xCore plugin.yml and test code

--- xCore/src/main/java/de/xearox/xplugin/MainClass.java
+++ xCore/src/main/java/de/xearox/xplugin/MainClass.java
@@ -4,4 +4,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MainClass extends JavaPlugin {
 
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+	
+	public void sendMessageToConsole(){
+		System.out.println("Aufruf aus einer anderen Jar heraus");
+	}
 }
