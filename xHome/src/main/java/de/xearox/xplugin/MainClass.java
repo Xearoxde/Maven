9704731f1c0bd6baecfef1fc8c2b1065967a2809
Xearox <support@xearox.de>
2015-11-17 09:23:23 +0100

    added some testing code to MainClass

--- xHome/src/main/java/de/xearox/xplugin/MainClass.java
+++ xHome/src/main/java/de/xearox/xplugin/MainClass.java
@@ -4,6 +4,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MainClass extends JavaPlugin{
 	
+	UtilClass utClass = new UtilClass();
+	FunctionsClass functionClass = new FunctionsClass();
+	SetLanguageClass langClass = new SetLanguageClass();
 	
+	@Override
+	public void onEnable(){
+		utClass.createLanguageFiles(this);
+	}
+	
+	@Override
+	public void onDisable(){
+		//
+	}
 
 }
