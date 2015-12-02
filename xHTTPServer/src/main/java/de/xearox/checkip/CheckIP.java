1de86e289862352cea3862b6d2e07d659b41da4c
Xearox <support@xearox.de>
2015-12-02 15:18:23 +0100

    Added external IP Getter

---
+++ xHTTPServer/src/main/java/de/xearox/checkip/CheckIP.java
@@ -0,0 +1,29 @@
+package de.xearox.checkip;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class CheckIP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		URL whatismyip;
+		try {
+			whatismyip = new URL("http://checkip.amazonaws.com");
+			BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+			String ip = in.readLine();
+			System.out.println(ip);
+		
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
